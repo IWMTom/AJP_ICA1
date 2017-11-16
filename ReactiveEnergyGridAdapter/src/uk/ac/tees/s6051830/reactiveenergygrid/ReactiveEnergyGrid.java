5c2033cf8c01839e5cf91d090f9511948fcc333e
Tom Wilson <tom@iwm.pw>
2017-11-16 22:06:53 +0000

    Generated project

---
+++ ReactiveEnergyGridAdapter/src/uk/ac/tees/s6051830/reactiveenergygrid/ReactiveEnergyGrid.java
@@ -0,0 +1,20 @@
+/*
+ * ReactiveEnergyGrid.java
+ */
+package uk.ac.tees.s6051830.reactiveenergygrid;
+
+/**
+ * @author Tom Wilson (S605130)
+ */
+public class ReactiveEnergyGrid
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
